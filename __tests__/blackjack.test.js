912888950d286a8f057d7f9ad18e6f39359970a7
chloewong108 <chloewng@ymail.com>
2022-12-29 17:01:42 +0000

    scenario one complete

--- __tests__/blackjack.test.js
+++ __tests__/blackjack.test.js
@@ -1,4 +1,4 @@
-const myDeck = require("../blackjack");
+const { myDeck, dealTwoCards, getValue } = require("../blackjack");
 
 describe("Blackjack game", () => {
   describe("Scenario 1: When I am dealt an opening hand, I have 2 cards:", () => {
@@ -16,6 +16,19 @@ describe("Blackjack game", () => {
       expect(testDeck).toContain("2 of diamonds");
       expect(testDeck).toContain("K of spades");
       expect(testDeck).toContain("8 of clubs");
+    });
+    test("should shuffle the array and give random cards", () => {
+      const testDeck1 = myDeck();
+      const testDeck2 = myDeck();
+      expect(testDeck1).not.toBe(testDeck2);
+    });
+    test("should be dealt 2 cards with opening hand ", () => {
+      const testGame = dealTwoCards();
+      expect(testGame.length).toBe(2);
+    });
+    test("should return the score with 10 if card is NaN, JQK", () => {
+      const testGame = getValue();
+      expect().toBe(10);
     });
   });
 });
--- blackjack.js
+++ blackjack.js
@@ -1,10 +1,6 @@
-// scernarios:
-//deck: [cards]
-//me: [2 cards]
-//make an array of cards
 function myDeck() {
   let deck = [];
-  let values = [
+  const values = [
     "A",
     "2",
     "3",
@@ -19,14 +15,46 @@ function myDeck() {
     "Q",
     "K",
   ];
-  let suits = ["clubs", "diamonds", "hearts", "spades"];
-
+  const suits = ["clubs", "diamonds", "hearts", "spades"];
   for (let i = 0; i < values.length; i++) {
     for (let j = 0; j < suits.length; j++) {
       deck.push(values[i] + " of " + suits[j]);
     }
   }
+  deck.sort(() => Math.random() - 0.5);
+  // sort() sorts the array out randomly, Math.random() gives a random number between 0-1, -0.5 will give a value of less than 0, 0 or greater than zero this determines the positioning of the cards.
   return deck;
 }
 
-module.exports = myDeck;
+function dealTwoCards() {
+  let myOpeningHand = [];
+
+  const getCard1 = myDeck().pop();
+  const getCard2 = myDeck().pop();
+  myOpeningHand.push(getCard1, getCard2);
+
+  return myOpeningHand;
+}
+
+function getValue() {
+  let score = 0;
+
+  const data = dealTwoCards();
+  const cardOne = data[0].split("of");
+  const cardTwo = data[1].split("of");
+
+  const value1 = cardOne[0];
+  const value2 = cardTwo[0];
+  console.log(value1);
+  if (isNaN(value1)) {
+    score += 10; // -> JQK
+  } else {
+    if (value1 === "A") {
+      score += 11;
+    }
+    score += parseInt(value1); // -> changes string to number
+  }
+  return score;
+}
+
+module.exports = { myDeck, dealTwoCards, getValue };
